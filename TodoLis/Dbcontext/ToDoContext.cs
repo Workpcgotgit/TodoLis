21e84e95ae89e5f4bd410ebaee9be087ff7cc29c
dev\shrinidhi.g <shrinidhi.g@sitero.com>
2025-03-18 15:25:29 +0530

    hello World

---
+++ TodoLis/Dbcontext/ToDoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ToDoDbContext : DbContext
+{
+	public DbSet<ToDoItem> ToDoItems { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer("Server=SITERO-PF3N194A;Database=ToDoListDb;Trusted_Connection=True;TrustServerCertificate=True;");
+	}
+}
---
+++ TodoLis/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class ToDoItem
+{
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(255)]
+	public string Title { get; set; } = string.Empty;
+
+	public bool IsCompleted { get; set; }
+
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
